d5d968859dba0d2c893df2652a48268263eb5657
k4x1 (coolest guy) <kazuoandrade@gmail.com>
2025-01-18 20:21:31 +1300

    sword

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Camera mainCamera;
+
+    [SerializeField] private float maxSpeed = 10f;
+    [SerializeField] private float acceleration = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+    }
+
+    private void FixedUpdate()
+    {
+        RotateTowardsMouse();
+        HandleMovement();
+        LimitSpeed();
+    }
+
+    private void RotateTowardsMouse()
+    {
+        Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - (Vector2)transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+
+        float rotation = Mathf.LerpAngle(rb.rotation, angle, rotationSpeed * Time.fixedDeltaTime);
+        rb.rotation = rotation;
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.up * acceleration);
+        }
+    }
+
+    private void LimitSpeed()
+    {
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+}
+
+
